5d0ad885ca81d126cf85d110e9b6e71aff89c3f4
lukaghogho <lukaghoghoberidze13@gmail.com>
2023-06-30 09:42:51 +0400

    Modal dynamic transition update

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 import styles from "./Modal.module.css";
 import Logout from "../logout/Logout";
 import Login from "../login/Login";
+import { useSpring, animated } from "@react-spring/web";
 
 const Modal = (props) => {
   const [content, setContent] = useState();
+  const [springs, api] = useSpring(() => ({
+    from: { opacity: props.modal ? 0 : 1 },
+    to: { opacity: props.modal ? 1 : 0 },
+  }));
   useEffect(() => {
     setContent(() => {
       if (props.type === "login")
@@ -12,16 +17,24 @@ const Modal = (props) => {
       else if (props.type === "logout")
         return <Logout modal={props.modal} setModal={props.setModal} />;
     });
-  }, []);
+  }, [props.type]);
   return (
-    <div className={styles.box}>
+    <animated.div className={styles.box} style={{ ...springs }}>
       {content}
       <ion-icon
-        onClick={() => props.setModal(false)}
+        onClick={() => {
+          api.start({
+            from: { opacity: 1 },
+            to: { opacity: 0 },
+          });
+          setTimeout(() => {
+            props.setModal(false);
+          }, 400);
+        }}
         id={styles.icon}
         name="close-circle"
       ></ion-icon>
-    </div>
+    </animated.div>
   );
 };
 
--- src/pages/sign-up/SignUp.jsx
+++ src/pages/sign-up/SignUp.jsx
@@ -1,5 +1,53 @@
+import styles from "./Signup.module.css";
+import { useSpring, useTransition, animated } from "@react-spring/web";
+import { useState, Fragment } from "react";
+
 const SignUp = () => {
-  return <form></form>;
+  const [color, setColor] = useState(false);
+  const [springs, api] = useSpring(() => ({
+    // from: { color: color ? "red" : "blue" },
+    // to: { color: color ? "blue" : "red" },
+  }));
+
+  const handleClick = () => {
+    api.start({
+      from: {
+        color: color ? "red" : "blue",
+      },
+      to: {
+        color: color ? "blue" : "red",
+      },
+    });
+    setColor((prev) => !prev);
+  };
+  return (
+    // <form className={styles.box}>
+    //   <p className={styles.text}>ZDRASTI</p>
+    // </form>
+    <Fragment>
+      <button onClick={handleClick}>Change color</button>
+      <animated.p
+        style={{
+          fontSize: "4.8rem",
+          color: "red",
+          transition: "color, .3s",
+          ...springs,
+        }}
+      >
+        BARCELONA
+      </animated.p>
+      {/* <animated.div
+        onClick={handleClick}
+        style={{
+          width: 80,
+          height: 80,
+          background: "#ff6d6d",
+          borderRadius: 8,
+          ...springs,
+        }}
+      /> */}
+    </Fragment>
+  );
 };
 
 export default SignUp;
